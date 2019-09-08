df1ef92b68b7a94d3be30c67fe9855b2feac1a7c
Cyrus Tau <cyrus.tau@gmail.com>
2019-09-08 13:25:34 -0700

    Fix bug

--- test_ard.py
+++ test_ard.py
@@ -1,8 +1,9 @@
 from serial import Serial
 from time import sleep
-s = Serial('/dev/ttyUSB0', 9600, timeout=100)
+s = Serial('/dev/ttyUSB0', 9600, timeout=10)
 s.rtscts = True
 throt = "t100\n"
 s.write(throt.encode("utf-8"))
 steer = "s200\n"
 s.write(steer.encode("utf-8"))
+s.close()
