3210c85026bc351545026d5d31cccf44f9f1f0df
Cyrus Tau <cyrus.tau@gmail.com>
2019-09-08 13:09:33 -0700

    Fix bug

--- test_ard.py
+++ test_ard.py
@@ -1,5 +1,8 @@
 from serial import Serial
 s = Serial('/dev/ttyUSB0', 9600, timeout=100)
 while True:
+    s.rtscts = True
     throt = "t100\n"
     s.write(throt.encode("utf-8"))
+    steer = "s60\n"
+    s.write(steer.encode("utf-8"))
