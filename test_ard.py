3bf6edd76cacf2fbd86669511f579d0cca7404a5
Akiyoshi Koide <AkoTako@Lings-MacBook-Pro.local>
2019-10-03 18:11:23 -0700

    Test ard

--- test_ard.py
+++ test_ard.py
@@ -2,9 +2,8 @@ from serial import Serial
 from time import sleep
 s = Serial('/dev/ttyUSB0', 9600, timeout=10)
 s.rtscts = True
-throt = "t100\n"
+throt = "t120\n"
 s.write(throt.encode("utf-8"))
 steer = "s200\n"
 s.write(steer.encode("utf-8"))
-s.flush()
 s.close()
