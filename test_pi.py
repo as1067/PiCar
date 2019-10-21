2b490b6843318f1c12fae86587e11283e2ff3cfc
Cyrus Tau <cyrus.tau@gmail.com>
2019-10-21 16:54:50 -0700

    New model

--- test_pi.py
+++ test_pi.py
@@ -4,9 +4,11 @@ GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical lay
 
 # Set up pin 11 for PWM
 GPIO.setup(40,GPIO.OUT)  # Sets up pin 11 to an output (instead of an input)
-p = GPIO.PWM(40, 50)     # Sets up pin 11 as a PWM pin
+p = GPIO.PWM(40, 50)# Sets up pin 11 as a PWM pin
+GPIO.setup(38,GPIO.OUT)
+q = GPIO.PWM(38,50)
 p.start(0)               # Starts running PWM on the pin and sets it to 0
-
+q.start(12)
 # Move the servo back and forth
 for i in range(20):
     print(i)
